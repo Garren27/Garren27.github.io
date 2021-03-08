2ba4f954c9eeb580e0b39a36d940898336f498ea
Garren27 <gmccall5@uwo.ca>
2021-03-07 23:26:52 -0500

    Started markup for Shopping List

---
+++ js/database.js
@@ -0,0 +1,19 @@
+
+
+/*
+function addItem(database){
+	database.ref('test').set({
+    chunga: "yunga"
+  });
+}
+
+function getItem(database){
+	var testValue = database.ref('test/chunga');
+	testValue.on('value', (snapshot) => {
+  		document.getElementById("result").innerHTML = snapshot.val();
+	});
+
+}
+
+*/
+
